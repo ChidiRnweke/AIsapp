3b1a6ccc6b8187702b5446d6fcb757b21301eca5
Chidi Nweke <chidi125@gmail.com>
2023-07-24 11:09:46 +0200

    Remove unused imports

--- app/aisapp/urls.py
+++ app/aisapp/urls.py
@@ -1,7 +1,6 @@
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from aisapp.views.user import UserViewSet
 from aisapp.views.tokens import CookieTokenObtainPairView, CookieTokenRefreshView
