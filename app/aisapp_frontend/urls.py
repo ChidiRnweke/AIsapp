b4c3a87ad7bd27be851e68a5b9d6e170027731ab
Chidi Nweke <chidi125@gmail.com>
2023-08-01 11:29:36 +0200

    Rename home to index.html

--- app/aisapp_frontend/urls.py
+++ app/aisapp_frontend/urls.py
@@ -3,6 +3,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     re_path(
-        r"^.*$", TemplateView.as_view(template_name="aisapp/home.html"), name="home"
+        r"^.*$", TemplateView.as_view(template_name="aisapp/index.html"), name="index"
     )
 ]
