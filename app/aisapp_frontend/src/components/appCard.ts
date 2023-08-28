132ecef531643938f7710393ac1a692146c3f180
Chidi Nweke <chidi125@gmail.com>
2023-08-28 21:56:12 +0200

    Update appcard to use web component API better

--- app/aisapp_frontend/src/components/appCard.ts
+++ app/aisapp_frontend/src/components/appCard.ts
@@ -1,30 +1,54 @@
+import { getElementOrThrow } from "../utils/utils.js";
 class AppCard extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
+        this.render();
     }
 
-    connectedCallback(): void {
-        const reversed = this.hasAttribute('reversed');
-        this.render(reversed);
-    }
-
-    render(reversed: boolean): void {
+    render(): void {
         this.shadowRoot!.innerHTML = `
         <style>
             @import url('static/aisapp/css/AppCard.css');
         </style>
 
-            <article class="card ${reversed ? 'reverse' : ''}">
+            <article class="card">
                 <section class="text-content">
                     <slot name="title"></slot>
                     <slot name="text"></slot>
                 </section>
-                <img src="${this.getAttribute('img-src')}" alt="${this.getAttribute('img-alt')}">
+                <img>
 
             </article>
         `;
     }
+
+    static get observedAttributes() {
+        return ["img-src", "img-alt", "reversed"];
+    }
+
+    attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+        const imgElem = getElementOrThrow<HTMLImageElement>(this.shadowRoot!, 'img');
+        const article = getElementOrThrow<HTMLElement>(this.shadowRoot!, 'article');
+
+
+        switch (name) {
+
+            case 'img-src':
+                imgElem.setAttribute("src", newValue);
+                break;
+
+            case 'img-alt':
+                imgElem.setAttribute("alt", newValue);
+                break;
+
+            case 'reversed':
+                article.className = "card reverse";
+                break;
+
+        }
+    }
+
 }
 
 customElements.define('app-card', AppCard);
