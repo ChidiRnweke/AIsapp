11bf9d42e24a5d53390060a3d33832f2f0c0ef15
ChidiRNweke <chidi125@gmail.com>
2024-02-22 06:59:34 +0100

    Remove all Python and frontend code
    These will receive a full rework from A-Z

--- app/aisapp/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class AisappConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "aisapp"
-
-    def ready(self) -> None:
-        import aisapp.signals  # noqa: F401
--- app/frontend/src/pages/index.ts
+++
@@ -1,6 +0,0 @@
-export { homePage } from "./home.js";
-export { loginPage } from "./login.js";
-export { notFoundPage } from "./notFound.js";
-export { registerPage } from "./register.js";
-export { setupPage } from "./setup.js";
-export type { Page } from "./utils.js"
