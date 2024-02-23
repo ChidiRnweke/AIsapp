a5b6a4394f541961c3827e10335e170a5557ab6b
ChidiRNweke <chidi125@gmail.com>
2024-02-23 13:08:12 +0100

    Properly integrate svelte

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}', "./index.html", './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}', "./node_modules/flowbite-svelte-icons/**/*.{html,js,svelte,ts}",
+  content: ['./src/**/*.{html,js,svelte,ts}', "./src/app.html", './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}', "./node_modules/flowbite-svelte-icons/**/*.{html,js,svelte,ts}",
   ],
   plugins: [require('flowbite/plugin')],
 
@@ -15,16 +15,16 @@ export default {
         primary: '#1a73e8',
         secondary: '#0f62fe',
         accent: '#4db8ac',
-        background: '#EDF3FD',
-        'background-alt': '#FFFFFF',
+        background: '#FFFFFF',
+        'background-alt': '#EDF3FD',
         foreground: '#333333',
         subtle: '#606060',
         error: '#dc3545',
         'dark-primary': '#BB86FC',
         'dark-accent': '#03DAC5',
         'dark-secondary': '#C792EA',
-        'dark-background': '#2C2C2C',
-        'dark-background-alt': '#1F1B24',
+        'dark-background': '#1F1B24',
+        'dark-background-alt': '#2C2C2C',
         'dark-foreground': '#ffffff',
         'dark-subtle': '#b0b0b0',
         'dark-error': '#ff6b6b'
