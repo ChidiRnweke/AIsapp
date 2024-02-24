843303f41b8f7504f606387b816a725c2194bc50
ChidiRNweke <chidi125@gmail.com>
2024-02-24 08:18:02 +0100

    Turn the Form component into an actual form

--- frontend/src/components/Register/FormComponent.svelte
+++ frontend/src/components/Register/FormComponent.svelte
@@ -1,7 +1,7 @@
-<div class="grid grid-cols-1 place-items-center">
+<form class="grid grid-cols-1 place-items-center">
 	<section
 		class="dark:bg-dark-background-alt bg-background-alt flex w-full max-w-xl grid-cols-1 flex-col place-items-center rounded-md bg-opacity-30 p-16 shadow-sm dark:bg-opacity-50"
 	>
 		<slot />
 	</section>
-</div>
+</form>
