cd3f8cb87adb0afaf683cf87c619266177213992
ChidiRNweke <chidi125@gmail.com>
2024-02-24 18:05:35 +0100

    Add prop for styling to form component

--- frontend/src/components/Register/FormComponent.svelte
+++ frontend/src/components/Register/FormComponent.svelte
@@ -1,7 +1,25 @@
+<script lang="ts">
+	export let padding = '16';
+	export let formSize: 'sm' | 'md' | 'lg' = 'lg';
+
+	$: size = (() => {
+		switch (formSize) {
+			case 'sm':
+				return '';
+			case 'md':
+				return 'flex w-full max-w-md';
+			case 'lg':
+				return 'flex w-full max-w-xl';
+			default:
+				return '';
+		}
+	})();
+
+	$: sectionClass = `dark:bg-dark-background-alt bg-background-alt ${size} grid-cols-1 flex-col place-items-center rounded-md bg-opacity-30 p-${padding} shadow-sm dark:bg-opacity-50`;
+</script>
+
 <form class="grid grid-cols-1 place-items-center">
-	<section
-		class="dark:bg-dark-background-alt bg-background-alt flex w-full max-w-xl grid-cols-1 flex-col place-items-center rounded-md bg-opacity-30 p-16 shadow-sm dark:bg-opacity-50"
-	>
+	<section class={sectionClass}>
 		<slot />
 	</section>
 </form>
